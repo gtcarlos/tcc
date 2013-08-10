653152f08c3d6211194cc7c2564b9f20815d6db4
Guilherme Carlos <guilherme.thcarlos@gmail.com>
2013-08-09 21:44:31 -0300

    Inserting roles_mask in User view

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130808004628) do
+ActiveRecord::Schema.define(:version => 20130809221114) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(:version => 20130808004628) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.integer  "roles_mask"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
