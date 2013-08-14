6093245f192dd57e368b17809a791a9b823c7515
gtcarlos <guilherme.thcarlos@gmail.com>
2013-08-14 18:30:10 -0300

    Changing index layout

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,10 @@
 class HomeController < ApplicationController
   def show
-    if user_signed_in?
-      redirect_to users_path
-    else
+   if user_signed_in?
+     # redirect_to users_path
+     render "home/index"
+   else
       render "home/index"
-    end
+   end
   end
 end
