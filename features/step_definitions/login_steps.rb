1a132b12ffaea554511e4a2a03765ff0cf3611ad
gtcarlos <guilherme.thcarlos@gmail.com>
2013-08-14 20:36:24 -0300

    Implementing Login Step Definition (incomplete)

---
+++ features/step_definitions/login_steps.rb
@@ -0,0 +1,56 @@
+Dado(/^que eu seja um usuário cadastrado$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Dado(/^eu esteja na página inicial$/) do
+  visit "/"
+end
+
+Quando(/^eu preencho o campo E\-mail e o campo Senha corretamente$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Quando(/^clico em Entrar$/) do |link|
+  click_link(link)
+end
+
+Então(/^eu devo ver a página inicial$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Quando(/^eu preencho o campo E\-mail incorretamente e o campo Senha corretamente$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Então(/^eu devo ver a página de cadastro de usuário$/) do
+  visit new_user_registration_path
+end
+
+Então(/^uma mensagem de erro$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Quando(/^eu preencho o campo E\-mail corretamente e o campo Senha corretamente$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Dado(/^que eu seja um usuário$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Quando(/^eu preencho o E\-mail e o campo Senha incorretamente$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Quando(/^eu preencho o campo E\-mail com um e\-mail não válido$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Quando(/^clico em entrar$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Então(/^eu devo ver uma mensagem de erro$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
