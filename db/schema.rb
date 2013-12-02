4454f68442ac3d7c806c5c86f647d3c2ede98317
gtcarlos <guilherme.thcarlos@gmail.com>
2013-12-02 02:01:58 -0200

    Payments mods

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131201033622) do
+ActiveRecord::Schema.define(:version => 20131201135924) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -160,6 +160,7 @@ ActiveRecord::Schema.define(:version => 20131201033622) do
     t.datetime "date"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+    t.integer  "user_id"
   end
 
   create_table "posts", :force => true do |t|
