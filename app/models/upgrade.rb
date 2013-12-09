f88a4bd2a84c968d19285fa78d113e80c9054f3b
Guilherme von Zuben Ricetto <gvonzubenricetto@bitbucket.com>
2013-12-09 21:23:50 -0200

    Fixing upgrade and credit card

--- app/models/upgrade.rb
+++ app/models/upgrade.rb
@@ -1,6 +1,6 @@
 class Upgrade < ActiveRecord::Base
 	attr_accessible :account_plan, :credit_card, :user_id
-	validates_presence_of :account_plan, :credit_card, , :user_id, presence => true
+	validates_presence_of :account_plan, :credit_card, :user_id, presence => true
 
 	belongs_to :user
 	
