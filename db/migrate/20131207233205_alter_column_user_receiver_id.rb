d8f830ddf931652a714a9ed8b15936708dceff83
matheush@bitbucket.org <fogodo@gmail.com>
2013-12-09 20:22:00 -0200

    Removing alter table messages

--- db/migrate/20131207233205_alter_column_user_receiver_id.rb
+++
@@ -1,6 +0,0 @@
-class AlterColumnUserReceiverId < ActiveRecord::Migration
-  def change
-  	remove_column :messages, :user_receiver_id
-  	add_column :messages, :user_receiver_id, :string
-  end
-end
