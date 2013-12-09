ae177e0f5942923e109f4fafb9efee9e5ee5c09f
matheush@bitbucket.org <fogodo@gmail.com>
2013-12-09 21:08:41 -0200

    Fixing Master

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131209220529) do
+ActiveRecord::Schema.define(:version => 20131209230335) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -240,6 +240,7 @@ ActiveRecord::Schema.define(:version => 20131209220529) do
     t.string   "credit_card"
     t.datetime "created_at",   :null => false
     t.datetime "updated_at",   :null => false
+    t.integer  "user_id"
   end
 
   create_table "users", :force => true do |t|
