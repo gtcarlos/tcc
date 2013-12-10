c2f04d373524b72b43385daa75907e7c0bb47888
Guilherme von Zuben Ricetto <gvonzubenricetto@bitbucket.com>
2013-12-10 13:46:17 -0200

    Products

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -2,6 +2,9 @@ class Startup < ActiveRecord::Base
   attr_accessible :area, :cnpj_duns, :contact, :fantasy_name, :files, :location, :logo, :message_limit, :city, :state
   validates_presence_of :area, :cnpj_duns, :contact, :fantasy_name, :location, :city, :state, presence => true
 
+  belongs_to :user
+  has_many :posts
+
   has_attached_file :logo, :styles => {:small => "100x100>" },
     :url => "/assets/startups/:id/:style/:basename.:extension",
     :path => ":rails_root/public/assets/startups/:id/:style/:basename.:extension"
