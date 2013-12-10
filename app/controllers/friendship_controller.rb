af903aea639bafa447a0ae27a838d45259fe5fc9
Guilherme Theodoro Carlos <guilherme.thcarlos@gmail.com>
2013-12-10 09:31:12 -0200

    Follow redirect fixed

--- app/controllers/friendship_controller.rb
+++ app/controllers/friendship_controller.rb
@@ -4,7 +4,7 @@ class FriendshipController < ApplicationController
     @friendship = current_user.friendships.build(friend_id: params[:friend][:friend_id])
 
     if @friendship.save
-      redirect_to user_path(@user), notice: 'Usuario seguido com sucesso'
+      redirect_to user_path(User.find(params[:friend][:friend_id])), :notice => 'Usuario seguido com sucesso'
     else
       redirect_to user_path(@friend_id), alert: 'Nao foi possivel seguir'
     end
@@ -14,6 +14,6 @@ class FriendshipController < ApplicationController
     @friend_id = params[:id]
     @friendship = current_user.friendships.find(:all, :conditions => ["friend_id = ?", @friend_id]).first
     @friendship.destroy
-    redirect_to user_path(@friend_id), :alert => "Usuario removido com sucesso"
+    redirect_to user_path(@friend_id), :notice => "Usuario removido com sucesso"
   end
 end
