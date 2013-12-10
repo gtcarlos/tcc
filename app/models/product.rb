175405fa73a9961dbcfb9c63dc7a7c45cec890cf
Guilherme von Zuben Ricetto <gvonzubenricetto@bitbucket.com>
2013-12-10 02:48:35 -0200

    Products fixed

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,11 @@
 class Product < ActiveRecord::Base
-  attr_accessible :description, :image, :name, :subtitle, :video
+  attr_accessible :description, :image, :name, :subtitle, :video, :photo, :photo_file_name, :user_id
+  belongs_to :user
+  belongs_to :startup
+  
+  has_attached_file :photo, :styles => {:thumbnail => "50x50>", :small => "150x150>"},
+    :default_url => "default_avatar.png",
+    :url => "/assets/products/:id/:style/:basename.:extension",
+    :path => ":rails_root/public/assets/products/:id/:style/:basename.:extension"
+    belongs_to :user
 end
---
+++ db/migrate/20131209233221_add_user_id_to_startup.rb
@@ -0,0 +1,5 @@
+class AddUserIdToStartup < ActiveRecord::Migration
+  def change
+    add_column :startups, :user_id, :integer
+  end
+end
